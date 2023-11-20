55a44160529ed9180dacdbcaad8cd766823283cc
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-11-20 17:52:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,15 +15,8 @@ function take_snapshot() {
 
 console.log("ml5 version : " , ml5.version) ;
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/wAeRoLyFz/model.json',modelLoaded) ;
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/m4Y5cn_3c/model.json',modelLoaded) ;
+
 function modelLoaded() {
     console.log("Model Loaded!!") ;
-}
-
-function speak() {
-    var synth = window.speechSynthesis ;
-    speak_data1 = "The First Prediction is " + prediction1 ;
-    speak_data2 = "And The Second Prediction Is " + prediction2 ;
-    var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2) ;
-    synth.speak(utterThis) ;
 }
